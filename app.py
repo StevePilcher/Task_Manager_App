f8447a3f4cb1159b761800d763f28743b0ed101b
Steve Pilcher <stevepilcheris@gmail.com>
2020-07-06 12:35:24 +0000

    Update

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster.djr3q.mongodb.net/task_manager?retryWrites=true&w=majority')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
 
 mongo = PyMongo(app)
 
